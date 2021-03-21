883a6d12f387ae9735ab257ba1065fe3649b725b
Navas <gabrielnavas@email.com>
2021-03-21 13:51:10 -0300

    models category and contact

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+  name = models.CharField(max_length=240) 
+
+class Contact(models.Model):
+  name = models.CharField(max_length=255)
+  last_name = models.CharField(max_length=255, blank=True)
+  phone = models.CharField(max_length=255)
+  email = models.CharField(max_length=255, blank=True)
+  created_at = models.DateField(default=timezone.now)
+  description = models.CharField(max_length=255) 
+  category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
