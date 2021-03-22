752a42b7e674d765633af5ebd555996ff48fdb99
Navas <gabrielnavas@email.com>
2021-03-22 20:48:36 -0300

    Raise http 404 with get object or 404

--- contacts/views.py
+++ contacts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Contact
 
@@ -10,10 +10,8 @@ def index(request):
 
 
 def show_contact(request, contact_id):
-  try:
-    contact = Contact.objects.get(id=contact_id)
-    return render(request, 'contacts/show_contact.html', {
-      'contact': contact
-    })
-  except Contact.DoesNotExist as e:
-    raise Http404()
+  contact = get_object_or_404(Contact, id=contact_id)
+  return render(request, 'contacts/show_contact.html', {
+    'contact': contact
+  })
+  
