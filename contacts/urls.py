1bc15a08032cc2924fd43863920a5db0bc4967bb
Navas <gabrielnavas@email.com>
2021-03-23 01:02:02 -0300

    filter with name and last name

--- contacts/urls.py
+++ contacts/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
   path('', views.index, name='contacts.index'),
   path('<int:contact_id>', views.show_contact, name='contacts.show_contact'),
+  path('search/', views.search, name='contacts.search'),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.core.paginator import Paginator
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
+
 from .models import Contact
 
 def index(request):
@@ -21,3 +24,17 @@ def show_contact(request, contact_id):
     'contact': contact
   })
   
+
+def search(request):
+  term=request.GET.get('term')
+  fields = Concat('name', Value(' '), 'last_name')
+  contacts = Contact.objects.annotate(name_full=fields).filter(
+    name_full__icontains=term
+  )
+  print(contacts.query)
+  print(term)
+  paginator = Paginator(contacts, 3)
+  contacts_paginator = paginator.get_page(term)
+  return render(request, 'contacts/search.html', {
+    'contacts': contacts_paginator
+  })
