54bc4132ad020b7d1d9628ad93abe53374aec4f5
Navas <gabrielnavas@email.com>
2021-03-22 22:00:08 -0300

    pagination

--- contacts/views.py
+++ contacts/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
+from django.core.paginator import Paginator
 from .models import Contact
 
 def index(request):
   contacts = Contact.objects.all()
+  paginator = Paginator(contacts, 3)
+  page=request.GET.get('p')
+  contacts_paginator = paginator.get_page(page)
   return render(request, 'contacts/index.html', {
-    'contacts': contacts
+    'contacts': contacts_paginator
   })
 
 
