ea33a864bd2d422b72917ee6d088e549741e599f
Navas <gabrielnavas@email.com>
2021-03-23 22:06:02 -0300

    messages

--- contacts/views.py
+++ contacts/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
 from django.core.paginator import Paginator
 from django.db.models import Q, Value
 from django.db.models.functions import Concat
+from django.contrib import messages
 
 from .models import Contact
 
@@ -27,14 +28,20 @@ def show_contact(request, contact_id):
 
 def search(request):
   term=request.GET.get('term')
-  if not term:
-    return index(request)
+  if term == None:
+    messages.add_message(request, messages.ERROR, 'give me a search...')
+    return redirect('contacts.index')
   fields = Concat('name', Value(' '), 'last_name')
   contacts = Contact.objects.annotate(name_full=fields).filter(
     Q(phone__icontains=term) | Q(name_full__icontains=term),
   )
   paginator = Paginator(contacts, 3)
   contacts_paginator = paginator.get_page(term)
+  if len(contacts) == 0:
+    messages.add_message(request, messages.INFO, f'Found 0 result, ...')
+    messages.add_message(request, messages.INFO, f'do it again with other key...')
+  else:
+    messages.add_message(request, messages.SUCCESS, f'Found {len(contacts)} results')
   return render(request, 'contacts/search.html', {
     'contacts': contacts_paginator
   })
--- contacts_django/settings.py
+++ contacts_django/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,3 +126,13 @@ STATICFILES_DIRS = [
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 MEDIA_URL = 'media/'
+
+# to messages
+
+MESSAGE_TAGS = {
+    constants.ERROR: 'alert-danger',
+    constants.WARNING: 'alert-warning',
+    constants.SUCCESS: 'alert-success',
+    constants.DEBUG: 'alert-info',
+    constants.INFO: 'alert-info',
+}
